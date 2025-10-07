57d27598ac18a1bd13bdb8493457e1b0d2eccff6
Malik Hammad Afgan <143062653+hammad200769@users.noreply.github.com>
2025-10-07 15:00:29 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+const button = document.getElementById('btn');
+const emailInput = document.getElementById('email');
+const errorIcon = document.getElementById('error-icon');
+const errorMessage = document.getElementById('error-message'); 
